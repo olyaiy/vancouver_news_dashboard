0b26e7b0dc3d986ad34dc33f03fe512d5c6d205d
olyaiy <alexolyaiy@gmail.com>
2024-09-24 14:51:20 -0700

    update sidebar

--- src/components/admin-panel/sidebar.tsx
+++ src/components/admin-panel/sidebar.tsx
@@ -1,5 +1,5 @@
 import Link from "next/link";
-import { PanelsTopLeft } from "lucide-react";
+import { Newspaper } from "lucide-react";
 
 import { cn } from "@/lib/utils";
 import { useStore } from "@/hooks/use-store";
@@ -31,7 +31,7 @@ export function Sidebar() {
           asChild
         >
           <Link href="/dashboard" className="flex items-center gap-2">
-            <PanelsTopLeft className="w-6 h-6 mr-1" />
+            <Newspaper className="w-6 h-6 mr-1" />
             <h1
               className={cn(
                 "font-bold text-lg whitespace-nowrap transition-[transform,opacity,display] ease-in-out duration-300",
@@ -40,7 +40,7 @@ export function Sidebar() {
                   : "translate-x-0 opacity-100"
               )}
             >
-              Brand
+              Pulse News
             </h1>
           </Link>
         </Button>
--- src/lib/menu-list.ts
+++ src/lib/menu-list.ts
@@ -30,7 +30,7 @@ type Group = {
 export function getMenuList(pathname: string): Group[] {
   return [
     {
-      groupLabel: "",
+      groupLabel: "Links",
       menus: [
         {
           href: "/dashboard",
@@ -41,61 +41,61 @@ export function getMenuList(pathname: string): Group[] {
         }
       ]
     },
-    {
-      groupLabel: "Contents",
-      menus: [
-        {
-          href: "",
-          label: "Posts",
-          active: pathname.includes("/posts"),
-          icon: SquarePen,
-          submenus: [
-            {
-              href: "/posts",
-              label: "All Posts",
-              active: pathname === "/posts"
-            },
-            {
-              href: "/posts/new",
-              label: "New Post",
-              active: pathname === "/posts/new"
-            }
-          ]
-        },
-        {
-          href: "/categories",
-          label: "Categories",
-          active: pathname.includes("/categories"),
-          icon: Bookmark,
-          submenus: []
-        },
-        {
-          href: "/tags",
-          label: "Tags",
-          active: pathname.includes("/tags"),
-          icon: Tag,
-          submenus: []
-        }
-      ]
-    },
-    {
-      groupLabel: "Settings",
-      menus: [
-        {
-          href: "/users",
-          label: "Users",
-          active: pathname.includes("/users"),
-          icon: Users,
-          submenus: []
-        },
-        {
-          href: "/account",
-          label: "Account",
-          active: pathname.includes("/account"),
-          icon: Settings,
-          submenus: []
-        }
-      ]
-    }
+    // {
+    //   groupLabel: "Contents",
+    //   menus: [
+    //     {
+    //       href: "",
+    //       label: "Posts",
+    //       active: pathname.includes("/posts"),
+    //       icon: SquarePen,
+    //       submenus: [
+    //         {
+    //           href: "/posts",
+    //           label: "All Posts",
+    //           active: pathname === "/posts"
+    //         },
+    //         {
+    //           href: "/posts/new",
+    //           label: "New Post",
+    //           active: pathname === "/posts/new"
+    //         }
+    //       ]
+    //     },
+    //     {
+    //       href: "/categories",
+    //       label: "Categories",
+    //       active: pathname.includes("/categories"),
+    //       icon: Bookmark,
+    //       submenus: []
+    //     },
+    //     {
+    //       href: "/tags",
+    //       label: "Tags",
+    //       active: pathname.includes("/tags"),
+    //       icon: Tag,
+    //       submenus: []
+    //     }
+    //   ]
+    // },
+    // {
+    //   groupLabel: "Settings",
+    //   menus: [
+    //     {
+    //       href: "/users",
+    //       label: "Users",
+    //       active: pathname.includes("/users"),
+    //       icon: Users,
+    //       submenus: []
+    //     },
+    //     {
+    //       href: "/account",
+    //       label: "Account",
+    //       active: pathname.includes("/account"),
+    //       icon: Settings,
+    //       submenus: []
+    //     }
+    //   ]
+    // }
   ];
 }
